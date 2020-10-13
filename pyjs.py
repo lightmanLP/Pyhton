7b9e6ff58e2bbe57f3881d10d7fc517ad7df678e
lightmanLP <liteman1000@gmail.com>
2020-10-13 12:25:21 +0700

    Update pyjs.py

--- pyjs.py
+++ pyjs.py
@@ -3,24 +3,11 @@ from forbiddenfruit import curse
 
 Int = type("", (int,), {})
 Str = type("", (str,), {})
+document = type("", (), {"write": print})
 
-
-def add(a, b):
-  """  """
-
-  return str(Str(a) + Str(b))
-
-
-def sub(a, b):
-  """  """
-  
-  return int(Int(a) - Int(b))
-
-
-document = type('', (), {'write': print})
 for i in (int, str):
-  curse(i, "__add__", add)
-  curse(i, "__sub__", sub)
+  curse(i, "__add__", lambda a, b: str(Str(a) + Str(b)))
+  curse(i, "__sub__", lambda a, b: int(Int(a) - Int(b)))
 
 document.write("1" - 1);
 document.write(2 + "1");
