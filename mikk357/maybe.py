d6706a504fd895488e3b04f1d990aad603dabc72
mikk357 <mikk357@yandex.com>
2020-11-29 21:30:13 +0300

    да я дурашка, нельзя наследоваться от булла

--- mikk357/maybe.py
+++ mikk357/maybe.py
@@ -1,6 +1,24 @@
 import random
 
 
-Maybe = type("Maybe", (bool,), {
+maybe = type("Maybe", (int,), {
     "__bool__": lambda s: bool(random.getrandbits(1))
-})()
+})
+
+Maybe = maybe()
+
+
+if __name__ == "__main__":
+    hexdigits = "0123456789ABCDEF"
+
+    def char():
+        for i in hexdigits[1:]:
+            if Maybe:
+                if Maybe:
+                    return i
+        else:
+            return hexdigits[0]
+
+    for i in range(4):
+        print(f"{char()}{char()} ", end="")
+    print()
